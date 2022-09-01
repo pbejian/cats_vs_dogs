a5b0b9289c5cf72d971fb48d0cc40bce35186bf6
Pierre BEJIAN <pierre@bejian.fr>
2022-09-01 17:51:38 +0200

    tralalalalala

--- include.py
+++ include.py
@@ -39,32 +39,10 @@ def prediction(filename):
     result = loaded_model.predict(images)    
     result = result[0][0]
     if round(result) == 0:
-        return "chat 🐱 !"
+        return "chat 🐱"
     elif round(result) == 1:
-        return "chien 🐶 !"
+        return "chien 🐶"
     else:
         return "Erreur"
 
-
-def prediction(filename):
-    """
-    Renvoie la chaîne de caractère "chien" ou "chat" en fonction de la
-    prédiction faite par le modèle CNN. On rappelle que ce modèle déjà
-    entraîné est stocké dans le fichier "my_model.pkl".
-    """
-    model_filename = "my_model.pkl"
-    loaded_model = pickle.load(open(model_filename, 'rb'))
-    img = image.load_img(filename, target_size=(150, 150))
-    x = image.img_to_array(img)
-    x = x/255.
-    x = np.expand_dims(x, axis=0)
-    images = np.vstack([x])
-    result = loaded_model.predict(images)    
-    result = result[0][0]
-    if round(result) == 0:
-        return "chat"
-    elif round(result) == 1:
-        return "chien"
-    else:
-        return "Erreur"
 
